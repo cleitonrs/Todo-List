4ff703865b66cc6849082a4bbf6b699d9a743cc7
cleitonrs <rscleitonrs@gmail.com>
2022-11-14 14:49:27 -0300

    global css

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header"
+
+import './global.css'
+
+
+export function App() {
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <>
+      <div className={styles.header}></div>
+
+    </>
+  )
+}

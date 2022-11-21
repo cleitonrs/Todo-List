cbaa89b11368b1821c9645735375dad061e95d6d
cleitonrs <rscleitonrs@gmail.com>
2022-11-21 15:49:08 -0300

    responsive layout

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,59 +1,48 @@
 import styles from './List.module.css'
-
+import check from '../assets/check.svg'
+import { TbClipboardText, TbTrash } from 'react-icons/tb'
+import { BsFillCheckCircleFill } from 'react-icons/bs'
 
 export function List() {
   return (
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.created}>
-          <strong>Tarefas criadas</strong>
+          <p>Tarefas criadas</p>
           <span className={styles.created}>5</span>
         </div>
 
         <div className={styles.done}>
-          <strong>Concluídas</strong>
+          <p>Concluídas</p>
           <span className={styles.done}>2 de 5</span>
         </div>
       </div>
 
-      <div className={styles.task}>
-        <input type="checkbox" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
-        </p>
-        
-      </div>
+      <div className={styles.list}>
+        <div className={styles.task}>
+          <button className={styles.check}>
+            <img src={check} alt="" />
+          </button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
+          </p>
+          <button className={styles.delete}>
+            <TbTrash size={20} />
+          </button>
+        </div>
 
-      <div className={styles.task}>
-        <input type="checkbox" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
-        </p>
-        
-      </div>
+        <div className={styles.task}>
+          <button className={styles.check}>
+            <img src={check} alt="" />
+          </button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
+          </p>
+          <button className={styles.delete}>
+            <TbTrash size={20} />
+          </button>
+        </div>
 
-      <div className={styles.task}>
-        <input type="checkbox" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
-        </p>
-        
-      </div>
-
-      <div className={styles.task}>
-        <input type="checkbox" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
-        </p>
-        
-      </div>
-
-      <div className={styles.task}>
-        <input type="checkbox" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum libero dolor repellendus voluptas voluptatum. Autem fugit magnam ducimus nisi quaerat. Optio magnam distinctio.
-        </p>
-        
       </div>
     </div>
   )

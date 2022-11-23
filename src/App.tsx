fdaeed40f4349c1e26e8f72a7b220770738eb717
cleitonrs <rscleitonrs@gmail.com>
2022-11-23 17:50:35 -0300

    save todos on localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Header } from "./components/Header"
 import { List } from "./components/List"
 import { v4 as uuidv4 } from "uuid"
 import './global.css'
+
+const  SAVED_TODOS = "savedTodos"
 
 export interface TodosType {
   id: string
@@ -11,25 +13,30 @@ export interface TodosType {
 }
 
 export function App() {
-  const [todos, setTodos] = useState<TodosType[]>([
-    {
-      id: "teste",
-      title: "Testando a aplicação",
-      isCompleted: true,
-    },
-    {
-      id: "teste2",
-      title: "Testando a aplicação novamente",
-      isCompleted: false,
+  const [todos, setTodos] = useState<TodosType[]>([])
+
+  function saveTodos(newTodos: TodosType[]) {
+    setTodos(newTodos)
+    localStorage.setItem(SAVED_TODOS, JSON.stringify(newTodos))
+  }
+
+  function loadSavedTodos() {
+    const saved = localStorage.getItem(SAVED_TODOS)
+    if (saved) {
+      setTodos(JSON.parse(saved))
     }
-  ])
+  }
+
+  useEffect(() => {
+    loadSavedTodos()
+  },[])
 
   function addTodo(todoTitle: string) {
     // Validation for no text or white space with regex
     if (!todoTitle || /^\s*$/.test(todoTitle)){
       return
     }
-    setTodos([
+    saveTodos([
       ...todos,
       {
         id: uuidv4(),
@@ -41,7 +48,7 @@ export function App() {
 
   function deleteTodo(todoId: string) {
     const newTodos = todos.filter((todo) => todo.id !== todoId )
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   function toggleCompletedTodo(todoId: string) {
@@ -54,7 +61,7 @@ export function App() {
     }
       return todo
     })
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   return (
